9959ec746deae9f36394f250f67e4bf70b55c4a2
Nelson Martins <nelsontecti@gmail.com>
2018-06-15 21:05:43 -0300

    Novo Projeto Mensagem_de_progresso_de_arquivo

---
+++ Mensagem_de_progresso_de_arquivo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Mensagem_de_progresso_de_arquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caminhoOrigem = @"C:\Users\nmartin\Downloads\origem";
+
+            var caminhoDestino = @"C:\Users\nmartin\Downloads\destino";
+
+            try
+            {
+                Console.WriteLine("Iniciando a cópia do arquivo");
+
+                /*
+                 * AllDialogs	Mostra a caixa de diálogo de progresso e qualquer caixa de 
+                 * diálogo de erro
+                 * 
+                 * OnlyErrorDialogs	Somente mostrar caixas de diálogo de erro e ocultar as 
+                 * caixas de diálogo de progresso. Padrão
+                 */
+                FileSystem.CopyDirectory(caminhoOrigem, caminhoDestino, UIOption.AllDialogs);
+                
+                /*
+                 * Esse método copia o conteúdo da pasta de origem para o diretório de destino. 
+                 * Se o diretório de destino não existir, ele é criado. Se um diretório com o 
+                 * mesmo nome existir no local de destino, o conteúdo dos dois diretórios serão 
+                 * mesclados. Você pode especificar um novo nome para o diretório durante a 
+                 * operação.
+                 */
+                //FileSystem.CopyDirectory(caminhoOrigem, caminhoDestino, true);
+
+                Console.WriteLine("Arquivo copiado com sucesso !");
+                Console.ReadKey();
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Cópia de arquivo cancelada !");
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
---
+++ Mensagem_de_progresso_de_arquivo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mensagem_de_progresso_de_arquivo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mensagem_de_progresso_de_arquivo")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b7a74fe-d5ea-485a-8ee5-94403cb97a2c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
